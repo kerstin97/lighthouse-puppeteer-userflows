5edf3e94b04515dcc612ca662807ba5ce72cb986
kerstin97 <kerstin.reichinger@gmx.at>
2023-04-06 11:47:27 +0200

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,18 @@
+# User Flow Test with Puppeteer and Lighthouse
+
+Following steps are in the flow:
+
+- Visit homepage
+- View all events
+- View event detail
+- Login
+- Create Event
+- View My Events
+
+## Run test without lighthouse
+
+`node remix-test.js`
+
+## Run with lighthouse
+
+`node remix_test_with_lighthouse.js`
---
+++ remix-test.js
@@ -0,0 +1,111 @@
+import puppeteer from "puppeteer";
+
+const escapeXpathString = (str) => {
+  const splitedQuotes = str.replace(/'/g, `', "'", '`);
+  return `concat('${splitedQuotes}', '')`;
+};
+
+const clickByText = async (page, text) => {
+  const escapedText = escapeXpathString(text);
+  const linkHandlers = await page.$x(`//a[contains(text(), ${escapedText})]`);
+  const buttonHandlers = await page.$x(
+    `//button[contains(text(), ${escapedText})]`
+  );
+
+  if (linkHandlers.length > 0) {
+    await linkHandlers[0].click();
+  } else if (buttonHandlers.length > 0) {
+    await buttonHandlers[0].click();
+  } else {
+    throw new Error(`Link not found: ${text}`);
+  }
+};
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+    args: [`--window-size=1350,940`],
+    defaultViewport: {
+      width: 1350,
+      height: 940,
+    },
+  });
+  const page = await browser.newPage();
+
+  // Homepage
+  await page.goto("http://193.170.119.146");
+
+  // Wait for View all events link and navigate to All events
+  const showAllEvents = "a";
+  await page.waitForSelector(showAllEvents);
+
+  await flow.navigate(async () => {
+    await page.click(showAllEvents);
+  });
+
+  await page.waitForNavigation({ waitUntil: "load" });
+  console.log("Current page should be events feed:", page.url());
+
+  // All Events
+
+  // Wait for results and click on first event, navigate to event detail
+  const eventDetailLink = ".event-card-link";
+  await page.waitForSelector(eventDetailLink);
+  await page.click(eventDetailLink);
+  await page.waitForNavigation({ waitUntil: "load" });
+  console.log("Current page is event detail:", page.url());
+
+  // Event Detail
+
+  // Click on Login, navigate to login page
+  await clickByText(page, `Login`);
+  await page.waitForNavigation({ waitUntil: "load" });
+  console.log("Current page is login page:", page.url());
+
+  // Login
+
+  // fill form and send
+  await page.type("#username-input", "kersoleynsta");
+  await page.type("#password-input", process.env.PASSWORD);
+
+  await Promise.all([
+    page.$eval("form", (form) => form.submit()),
+    page.waitForNavigation(),
+  ]);
+
+  // successfully logged in
+
+  await page.waitForNavigation({ waitUntil: "load" });
+  console.log("Current page:", page.url());
+
+  // click on create event
+  await clickByText(page, `Event erstellen`);
+  await page.waitForNavigation({ waitUntil: "load" });
+  console.log("Current page:", page.url());
+
+  // Create Event
+  await page.waitForSelector("input[name=title]");
+  await page.type("input[name=title]", "pupeteer test");
+  await page.waitForSelector("textarea[name=info]");
+  await page.type("textarea[name=info]", "pupeteer test");
+  await page.waitForSelector("input[name=date]");
+  await page.type("input[name=date]", "25110020231000");
+  await page.waitForSelector("input[name=costs]");
+  await page.type("input[name=costs]", "5");
+  await page.waitForSelector("input[name=capacity]");
+  await page.type("input[name=capacity]", "5");
+
+  console.log("Current page:", page.url());
+  await clickByText(page, `Speichern`);
+
+  await page.waitForNavigation({ waitUntil: "load" });
+  console.log("Current page:", page.url());
+
+  // My Events
+  await clickByText(page, `Meine Events`);
+  await page.waitForNavigation({ waitUntil: "load" });
+  console.log("Current page:", page.url());
+
+  await browser.close();
+})();
